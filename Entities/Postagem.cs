b5352b229dc2ac5dac3c5f9afecec1782f9deb15
Jonatas Vitalino <jcv@cin.ufpe.br>
2020-01-16 23:36:26 -0300

    Troquei a classe Post por Postagem e adicionei e criei o Program

---
+++ Entities/Postagem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Post.Entities
+{
+    class Postagem
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Postagem() { }
+
+        public Postagem(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments : ");
+            foreach (Comment C in Comments)
+            {
+                sb.AppendLine(C.Text);
+            }
+
+            return sb.ToString();
+            
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Post.Entities;
+
 
 namespace Post
 {
@@ -6,7 +9,20 @@ namespace Post
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome");
+
+            Postagem p1 = new Postagem(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12
+                );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
         }
     }
 }
